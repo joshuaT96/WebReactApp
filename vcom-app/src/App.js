3340f0f8f3752c872614a1cce7cc0876df19dfc2
joshuaT96 <joshua@solareff.co.za>
2024-11-29 15:24:39 +0200

    added common components

--- vcom-app/src/App.js
+++ vcom-app/src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
+import SiteFooter from './components/common/SiteFooter';
+import SiteNav from './components/common/SiteNav';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Route, Routes} from 'react-router-dom'; 
+
+import HomePage from './components/home/HomePage';
+import LoginPage from './components/auth/LoginPage';
+import RegisterPage from './components/auth/RegisterPage';
+
 
 function App() {
   return (
     <div>
-      <header>
-
-      </header>
+      <SiteNav/>
+        <Routes>
+          <Route path='*' element={<HomePage/>}/>
+          <Route path='/' exact={true} element={<HomePage/>}/>
+          <Route path='/login' element={<LoginPage/>}/>
+          <Route path='/register' element={<RegisterPage/>}/>
+        </Routes>
+      <SiteFooter/>
     </div>
   );
 }
