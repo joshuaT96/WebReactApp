40e1487e186a3b30c45c78c203ae2939c49c9085
joshuaT96 <joshua@solareff.co.za>
2025-01-31 10:17:54 +0200

    connection to new dynamoDB established - web app can read data from new table

--- vcom-app/src/components/technicians/technicians.js
+++ vcom-app/src/components/technicians/technicians.js
@@ -1,7 +1,38 @@
+import React, { useEffect, useState } from 'react';
+import { listSolarPlants, listVCOMSMSTechnicianDetails } from '../../graphql/queries';
+import { updateSolarPlant } from '../../graphql/mutations';
+import { generateClient } from 'aws-amplify/api';
+import { Button } from 'react-bootstrap';
 import './techniciansStyles.css'
 
+const client = generateClient();
 
 const App = () => {
+
+    const [technicianDisplay, setTechs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetchTechnicians();
+        //console.log('page loaded')
+    })
+
+    const fetchTechnicians = async () => {
+        try {
+            const techData = await client.graphql({ query: listVCOMSMSTechnicianDetails})
+            const technicians = techData.data.listVCOMSMSTechnicianDetails.items;
+            setTechs(technicians);
+
+
+
+        } catch (err) {
+            console.error('Error fetching technician data', err)
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+
 
 
 
@@ -14,13 +45,19 @@ const App = () => {
             <table>
                 <thead>
                     <tr>
-                        <th className='timeslotColumn'>Timeslot</th>
                         <th className='codeColumn'>Code</th>
-                        <th className='technicianColumn'>Technician</th>
                         <th className='contactNumberColumn'>Contact Number</th>
+                        <th className='technicianColumn'>Technician</th>
                     </tr>
                 </thead>
                 <tbody>
+                {technicianDisplay.sort((a, b) => a.code - b.code).map((technician) => (
+                    <tr key={technician.code}>
+                        <td>{technician.code}</td>
+                        <td>{technician.cellNumber}</td>
+                        <td>{technician.name}</td>
+                    </tr>
+                ))}
 
                 </tbody>
             </table>
--- vcom-app/src/graphql/queries.js
+++ vcom-app/src/graphql/queries.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
+/* --------------------------------------SolarPlant-----------------------------------------------*/
 export const getSolarPlant = /* GraphQL */ `
   query GetSolarPlant($id: ID!) {
     getSolarPlant(id: $id) {
@@ -36,3 +37,56 @@ export const listSolarPlants = /* GraphQL */ `
     }
   }
 `;
+
+/* --------------------------------------VCOMSMSBESSData-----------------------------------------------*/
+export const listVCOMSMSBESSData = /* GraphQL */ `
+  query ListVCOMSMSBESSData(
+    $filter: TableVCOMSMSBESSDataFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listVCOMSMSBESSData(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        index
+        commsLost
+        commsLostCounter
+        genPowerAbbreviation
+        genRunning
+        key
+        plantName
+        sendSMS
+        soc
+        socAbbreviation
+        socMinLevel
+        
+        createdAt
+        updatedAt
+        __typename
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+
+
+/* --------------------------------------VCOMSMSTechnician-----------------------------------------------*/
+export const listVCOMSMSTechnicianDetails = /* GraphQL */ `
+  query ListVCOMSMSTechnicianDetails(
+    $filter: TableVCOMSMSTechnicianDetailsFilterInput
+    $limit: Int
+    $nextToken: String
+  ) {
+    listVCOMSMSTechnicianDetails(filter: $filter, limit: $limit, nextToken: $nextToken) {
+      items {
+        code
+        cellNumber
+        name
+
+      }
+      nextToken
+      __typename
+    }
+  }
+`;
+
